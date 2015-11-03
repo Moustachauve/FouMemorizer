f82d1b0cbd1e6e1981053958bbd3eea8ff604b71
Christophe Gagnier <admin@cgagnier.ca>
2015-11-03 14:03:29 -0500

    Login/register

---
+++ FouMemorizer/js/Application/ApplicationController.js
@@ -0,0 +1,55 @@
+﻿angular
+    .module('FouMemorizer')
+    .controller('ApplicationController', ['$scope', '$rootScope', ApplicationController]);
+
+function ApplicationController($scope, $rootScope) {
+
+    $scope.showLoading = false;
+
+    $scope.pages = [
+        { name: "index", title: "Accueil", url: "/js/Memo/MemoList.html" },
+        { name: "register", title: "S'inscrire", url: "/js/Auth/Register.html" },
+    ];
+
+    $scope.currentPage = $scope.pages[0];
+
+    $rootScope.startLoading = function () {
+        $scope.showLoading = true;
+    }
+
+    $rootScope.stopLoading = function () {
+        $scope.showLoading = false;
+    }
+
+    $rootScope.navigate = function (pageName) {
+        var page;
+        pageName = pageName.toLowerCase();
+
+        if (!pageName) {
+            page = $scope.pages[0];
+        }
+        else {
+            for (var i = 0; i < $scope.pages.length; i++) {
+                if ($scope.pages[i].name == pageName) {
+                    page = $scope.pages[i];
+                    break;
+                }
+            }
+        }
+        if (page === undefined)
+            return;
+
+        if ($scope.currentPage.name != page.name) {
+            $scope.currentPage = page;
+            location.hash = "/" + page.name;
+        }
+    };
+
+    $scope.$watch(function () {
+        return location.hash
+    }, function (value) {
+        console.log(location.hash.substr(2, location.hash.length));
+        $rootScope.navigate(location.hash.substr(2, location.hash.length));
+    });
+}
+
---
+++ FouMemorizer/js/Auth/AuthController.js
@@ -0,0 +1,40 @@
+﻿angular
+    .module('FouMemorizer')
+    .controller('AuthController', ['$scope', '$rootScope', AuthController]);
+
+function AuthController($scope, $rootScope) {
+
+    $scope.registerError = "";
+
+    $scope.register = function (registerForm) {
+        $rootScope.startLoading();
+
+        if (registerForm == null) {
+
+        }
+
+        data = {
+            Email: registerForm.Email,
+            Password: registerForm.Password,
+            ConfirmPassword: registerForm.ConfirmPassword
+        }
+
+        $.ajax({
+            method: "POST",
+            url: "http://localhost:3791/api/Account/Register",
+            data: data
+        })
+        .done(function (data) {
+            $rootScope.stopLoading();
+            $scope.memos = data;
+            $scope.$apply();
+        }).fail(function () {
+            $scope.serverError = true;
+            $rootScope.stopLoading();
+            $scope.$apply();
+        });
+
+        $scope.registerError = "Allo";
+    }
+
+}
